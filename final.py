3dd6cbb54689b5ea5aa4666ad94f06d18044c927
Spozder3 <pozdersa@s.dcsdk12.org>
2017-12-19 09:10:48 -0700

    I added a function to add movies even if they arent of the exact genre.

--- final.py
+++ final.py
@@ -1,49 +1,97 @@
-from sys import argv
-
 data = open("movies.csv", 'r')
 lines = data.readlines()
 
 genres = []
-titles = []
-stars = []
+perfect_match_titles = []
+related_titles = []
+perfect_match_stars = []
+related_stars = []
 
-def Your_Genre(lines):
-	prompt = "> "
-	print "What genre would you like to see? If it is several Genres then put a | between them. Also list the genres in alphabetical order. Ex: Comedy|Crime|Drama"
-	genre_answer = raw_input(prompt)
+prompt = "> "
+print "What genre would you like to see? If it is several Genres then put a | between them. Also list the genres in alphabetical order. Ex: Comedy|Crime|Drama"
+genre_answer = raw_input(prompt)
 
+print ""
+
+def Your_Exact_Genre(lines):
 	for i in range(1,len(lines)):
 		info = lines[i].rstrip().split(",")
 		genre = info[2]
 		title = info[1]
 		if genre.lower() == genre_answer.lower():
-			titles.append(title.lower())
+			perfect_match_titles.append(title.lower())
 
-	if len(titles) == 0:
-		return "Sorry, there are no movies with the genre",genre_answer,"try again."
-		sys.exit(argv)
+	if len(perfect_match_titles) == 0:
+		return "Sorry, there are no exact movies with the genre %s" % (genre_answer)
 	else:
-		print "Here are movies with that genre!"
-		return titles
+		print "Here are movies with that exact genre!"
+		return perfect_match_titles
 
-print Your_Genre(lines)
+print Your_Exact_Genre(lines)
 
-def Best_Movie(lines):
-	prompt = "> "
-	print "Would you like to see the rating for each movie?"
-	yes_or_no = raw_input(prompt)
+print ""
 
-	if yes_or_no == "yes":
-		for i in range(1,len(lines)):
-			info = lines[i].rstrip().split(",")
-			genre = info[2]
-			title = info[1]
-			star_rating = info[3]
-			for n in range(0,len(titles)):
-				if title.lower() == titles[n].lower():
-					stars.append(star_rating)
-		return stars
+def Related_Genre(lines):
+	for i in range(1,len(lines)):
+		info = lines[i].rstrip().split(",")
+		genre = info[2]
+		title = info[1]
+		if genre_answer.lower() in genre.lower():
+			if title.lower() not in perfect_match_titles:
+				related_titles.append(title.lower())
+
+	if len(related_titles) == 0:
+		return "Sorry, there are no movies with the genre %s try again" % (genre_answer)
 	else:
-		return "Okay, have fun watching!"
+		print "Here are some movies that are partially that genre!"
+		return related_titles
 
-print Best_Movie(lines)
+print Related_Genre(lines)
+
+print ""
+
+prompt = "> "
+print "Would you like to see the rating for each movie?"
+yes_or_no = raw_input(prompt)
+
+print ""
+
+def Best_Exact_Movies(lines):
+	if len(perfect_match_titles) == 0:
+		return "There are no exact genre movie ratings."
+	else:
+		if yes_or_no == "yes":
+			for i in range(1,len(lines)):
+				info = lines[i].rstrip().split(",")
+				genre = info[2]
+				title = info[1]
+				star_rating = info[3]
+				for n in range(0,len(perfect_match_titles)):
+					if title.lower() == perfect_match_titles[n].lower():
+						perfect_match_stars.append(star_rating)
+			return perfect_match_stars
+		else:
+			return "Okay, have fun watching!"
+
+print Best_Exact_Movies(lines)
+
+print ""
+
+def Best_Related_Genre_Movies(lines):
+	if len(related_titles) == 0:
+		return "There are no movie ratings with that genre at all."
+	else:
+		if yes_or_no == "yes":
+			for i in range(1,len(lines)):
+				info = lines[i].rstrip().split(",")
+				genre = info[2]
+				title = info[1]
+				star_rating = info[3]
+				for n in range(0,len(related_titles)):
+					if title.lower() == related_titles[n].lower():
+						related_stars.append(star_rating)
+			return related_stars
+		else:
+			return "Okay, have fun watching!"
+
+print Best_Related_Genre_Movies(lines)
